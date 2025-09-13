3bb3109a4aee63e68265e14566a013a2ecfe03ff
Ozai <ozai@Ozais-MacBook-Air.local>
2025-09-13 20:47:13 +0700

    p1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,11 @@
-import React from "react";
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-} from "./ui/navigation-menu";
-import { Link } from "react-router-dom";
+import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=" flex justify-between items-center p-4 bg-gray-800 text-white ">
-      <div>
-        <Link className=" flex items-center gap-3 " to="/">
-          <img
-            className=" w-15 "
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
-            alt="Logo"
-          />
-          <h1 className=" text-3xl ">React</h1>
-        </Link>
-      </div>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem className={"flex gap-2 "}>
-            <NavigationMenuLink asChild>
-              <Link to="/">Home</Link>
-            </NavigationMenuLink>
-            <NavigationMenuLink asChild>
-              <Link to="/product">Products</Link>
-            </NavigationMenuLink>
-            <NavigationMenuLink asChild>
-              <Link to="/about">About</Link>
-            </NavigationMenuLink>
-            <NavigationMenuLink asChild>
-              <Link to="/contact">Contact</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+    <div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
 
 const ProductCard = () => {
-
-    const products = [
-        {
-            id: 1,
-            image: ;
-            name: "" ,
-
-        }
-    ]
-
   return (
     <div>
       
