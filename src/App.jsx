504e8a95405ff2839840eb9f9134ae49556904c8
Vatnaa3301 <vatanaking20@gmail.com>
2025-11-16 16:23:50 +0700

    update font

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./font/kamerik.css";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./page/Home";
